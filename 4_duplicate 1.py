423d23fab53fcaa95ac82663cdc6b37037109be9
Carnage725 <chinmaytalnikar@gmail.com>
2025-10-13 08:10:23 +0530

    new notebook overall

--- 4_duplicate 1.py
+++
@@ -1,8 +0,0 @@
-# duplicate 1 implementation
-def duplicate1(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr)):
-            if i != j and arr[i] == arr[j]:
-                return True
-    return False
-
--- 5_duplicate 2.py
+++
@@ -1,9 +0,0 @@
-# duplicate 2 implementation
-def duplicate2(arr):
-    newArr = []
-    for i in range(len(arr)):
-        if(newArr[arr[i]] == 1):
-            return True
-        else:
-            newArr[arr[i]] = 1
-    return False
